0a937a39c1f54dc50ac62bcdf31e7b0cf13d3cff
Gaurav <gauravsoni2@KTRAININD.COM>
2021-11-14 00:05:16 +0530

    user login and register added

--- project_progress_tracking/urls.py
+++ project_progress_tracking/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(('projects.urls','projects'),namespace='projects')),
+    path('users/',include(('users.urls','users'),namespace='users')),
 ]
