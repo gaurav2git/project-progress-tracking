8bb835a96fa07e6ba79cad8af9eb3131da42c33a
Gaurav <gauravsoni2@KTRAININD.COM>
2021-11-14 17:59:51 +0530

    frontend - login register dashboard added

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from .models import Project
 
 # Create your views here.
+
+@login_required
 def index(request):
     """View for home/index page"""
     projects = Project.objects.order_by('date_added')
     content = {'projects':projects}
-    return render(request, 'index.html', content)
+    return render(request, 'dashboard.html', content)
