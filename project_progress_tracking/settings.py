63d16d45fb314f1e30362b4a532b062e42c5dbcc
Gaurav <gauravsoni2@KTRAININD.COM>
2021-11-17 19:16:26 +0530

    heroku settings added 7

--- project_progress_tracking/settings.py
+++ project_progress_tracking/settings.py
@@ -162,7 +162,7 @@ if os.getcwd() == '/app':
 
     BASE_DIR = os.path.dirname(os.path.dirname(__file__))
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     TEMPLATE_DIRS = (
         os.path.join(BASE_DIR,  'templates'),
     )
