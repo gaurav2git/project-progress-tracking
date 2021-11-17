7923981fc9cb02ea6b458b4925bccb0de948523b
Gaurav <gauravsoni2@KTRAININD.COM>
2021-11-17 18:45:10 +0530

    heroku settings added 3

--- project_progress_tracking/wsgi.py
+++ project_progress_tracking/wsgi.py
@@ -10,8 +10,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_progress_tracking.settings')
 
-application = Cling(get_wsgi_application())
+from whitenoise.django import DjangoWhiteNoise
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
